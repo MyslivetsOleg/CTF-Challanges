f90141e84cfc35d234069388b51a121a5bd10411
DESKTOP-SBFOFT0\Fakalor <myslivec.oleg@yandex.ru>
2022-02-02 00:13:21 +0300

    Minor fixes in PPC1.py

--- MF-CTF/PPC/PPC1/PPC1.py
+++ MF-CTF/PPC/PPC1/PPC1.py
@@ -1,42 +1,48 @@
 import socket
 import math
-import decimal
+from time import sleep
 
 
-def distance(points_coefs:list):
-    return math.sqrt(math.pow(points_coefs[2]-points_coefs[0],2) 
-                     + math.pow(points_coefs[3]-points_coefs[1],2))
+def parse_data(data):
+    buf_len = len(data)
+    values = ""
+    # read data from the end to find all 4 digits
+    for x in range(2, buf_len - 1):
+        if str(data[-x]) == '10': break
+        values += chr(data[-x])
+    values = values[::-1]
+    return values
 
-#trying to connect to MF-CTF
+
+def distance(points_coefs: list):
+    return math.sqrt(math.pow(points_coefs[2] - points_coefs[0], 2)
+                     + math.pow(points_coefs[3] - points_coefs[1], 2))
+
+
+# trying to connect to MF-CTF
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('ctf.mf.grsu.by', 9002))
-try:
-    counter = 0   
-    while (counter<=51):
-        if(counter == 51):
-             data = s.recv(1024)
-             print(data.decode())
-        print("Step: "+ str(counter))
-        data = s.recv(1024)
-        #coord in the end (format: x1,y1 -> x2,y2)
-        buf_len = len(data)
-        print(data.decode())
-        values ="";
-        #read data from the end to find all 4 digits
-        for x in range (2, buf_len-1):
-            if (str(data[-x])=='10'): break
-            values+=chr(data[-x])
-
-        values = values[::-1]    
+counter = 0
+flag_found = 0
+while (counter <= 51):
+    print("Step: " + str(counter))
+    sleep(1)
+    if (counter == 51):
+        data = s.recv(1024).decode('utf-8')
+        if 'grodno{' in data:
+            print(data)
+            flag_found = 1
+            break
+    data = s.recv(1024)
+    print(data.decode('utf-8'))
+    # coord in the end (format: x1,y1 -> x2,y2)
+    if flag_found != 1:
+        values = parse_data(data)
         points_coefs = [int(x) for x in values.split(';')]
         print(points_coefs)
-        d = decimal.Decimal(distance(points_coefs))
-        d = round(d,2)    
+        d = distance(points_coefs)
+        d = round(d, 2)
         print(d)
-        print(str(d).encode())
-        s.send(str(d).encode())        
-        counter+=1
-except Exception  as e:
-    print(e)
-finally:
-    s.close()
+        s.send((str(d) + '\r\n').encode())
+    counter += 1
+s.close()
